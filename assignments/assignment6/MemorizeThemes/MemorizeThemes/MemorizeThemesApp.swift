386332089dfbc54008f808c18c6f6b781c4e6bdc
Sofia Nadraga <103518690+sofianadraga@users.noreply.github.com>
2025-05-25 13:20:20 +0300

    Update MemorizeThemesApp.swift

--- assignments/assignment6/MemorizeThemes/MemorizeThemes/MemorizeThemesApp.swift
+++ assignments/assignment6/MemorizeThemes/MemorizeThemes/MemorizeThemesApp.swift
@@ -1,20 +1,11 @@
-//
-//  MemorizeThemesApp.swift
-//  MemorizeThemes
-//
-//  Created by Chenqin zhang on 2024/3/30.
-//
-
 import SwiftUI
 
 @main
 struct MemorizeThemesApp: App {
-//    @StateObject var game = EmojiMemoryGame(themeID: <#UUID#>, themeChooser: <#ThemeChooser#>)
     @StateObject var themeChooser = ThemeChooser()
     var body: some Scene {
         WindowGroup {
             ThemeChooserView(themeChooser: themeChooser)
-//                .environmentObject(<#T##object: ObservableObject##ObservableObject#>)
         }
     }
 }
