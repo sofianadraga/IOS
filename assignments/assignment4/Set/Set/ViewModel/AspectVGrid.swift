e87ef61b838b834cf5c0a11da4f8afdc6147846c
Sofia Nadraga <103518690+sofianadraga@users.noreply.github.com>
2025-05-25 13:14:48 +0300

    Update AspectVGrid.swift

--- assignments/assignment4/Set/Set/ViewModel/AspectVGrid.swift
+++ assignments/assignment4/Set/Set/ViewModel/AspectVGrid.swift
@@ -1,10 +1,3 @@
-//
-//  AspectVGrid.swift
-//  Memorize
-//
-//  Created by CS193p Instructor on 4/24/23.
-//
-
 import SwiftUI
 
 struct AspectVGrid<Item: Identifiable, ItemView: View>: View {
@@ -16,7 +9,6 @@ struct AspectVGrid<Item: Identifiable, ItemView: View>: View {
         self.items = items
         self.aspectRatio = aspectRatio
         self.content = content
-        //print(items.count)
     }
     
     var body: some View {
@@ -43,25 +35,19 @@ struct AspectVGrid<Item: Identifiable, ItemView: View>: View {
     ) -> CGFloat {
         let count = CGFloat(count)
         var columnCount = 1.0
-//        print("size: \(size)")
-//        print("size width: \(size.width)")
-//        print("count: \(count)")
+
         repeat {
             let width = size.width / columnCount
             let height = width / aspectRatio
             
             
             let rowCount = (count / columnCount).rounded(.up)
-            if rowCount * height < size.height {
-//                print("upper: \((size.width / columnCount).rounded(.down))")
+
                 return (size.width / columnCount).rounded(.down)
             }
             columnCount += 1
-//            print(columnCount)
         } while columnCount < count
-//        print("column count \(columnCount)")
-        
-//        print("gridItemWidth \(max(size.width / count, size.height * aspectRatio).rounded(.down))")
+
         return min(size.width / count, size.height * aspectRatio).rounded(.down)
     }
 }
