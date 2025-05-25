0b9ebb758398c326b92abf0d0aa88a049139759a
Sofia Nadraga <103518690+sofianadraga@users.noreply.github.com>
2025-05-25 13:16:59 +0300

    Update Extensions.swift

--- assignments/assignment5/Emoji Art L11/Emoji Art/Extensions.swift
+++ assignments/assignment5/Emoji Art L11/Emoji Art/Extensions.swift
@@ -1,11 +1,3 @@
-//
-//  Extension.swift
-//  Emoji Art
-//
-//  Created by CS193p Instructor on 5/8/23.
-//  Copyright (c) 2023 Stanford University
-//
-
 import SwiftUI
 
 typealias CGOffset = CGSize
@@ -29,12 +21,9 @@ extension CGOffset {
 }
 
 extension String {
-    // removes any duplicate Characters
-    // preserves the order of the Characters
+
     var uniqued: String {
-        // not super efficient
-        // would only want to use it on small(ish) strings
-        // and we wouldn't want to call it in a tight loop or something
+
         reduce(into: "") { sofar, element in
             if !sofar.contains(element) {
                 sofar.append(element)
